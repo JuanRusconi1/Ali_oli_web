09489d1cb6515f524adacf71db994278f0684c1c
Juan Rusconi <110208336+JuanRusconi1@users.noreply.github.com>
2023-02-07 00:13:29 -0300

    Update config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "phericsone_admin",
+    "password": "alioli2030",
+    "database": "phericsone_alioli",
+    "host": "phericsone.com.ar",
     "dialect": "mysql"
   }
 }
